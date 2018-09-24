ad22698e8b3c0df8fb4a8e085fdce029f96d3629
Jonne Huotari <{ID}+{username}@users.noreply.github.com>
2018-09-24 21:09:15 +0300

    added dataset

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import './bulma.min.css';
 import './App.css';
+
+import innovationProcurements from './all.json';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div className="columns">
+          <div className="column is-one-fifth">
+
+            <div className="App">
+              <h1 className="App-title">
+                Innovation procurements
+                <small>in EU</small>
+              </h1>
+            </div>
+
+          </div>
+          <div className="column">
+
+            here is map
+
+          </div>
+          <div className="column">
+
+            here is graph
+
+          </div>
+        </div>
+
+        <div className="columns">
+          <div className="column">
+            this is table
+
+            { innovationProcurements.map((data, key) => {
+              return(
+                <div key={key}>
+                  <b>{ data.tender_title }</b>
+                  <br />{ data.buyer_name }
+                  <br />
+                </div>
+              )
+            }) }
+          </div>
+        </div>
       </div>
     );
   }
