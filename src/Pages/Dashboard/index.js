6f6225a15581b6e0a70f6e17741f9c1469f40049
Perttu <perttu.lahteenlahti@helsinki.fi>
2018-09-26 17:52:08 +0300

    some changes

---
+++ src/Pages/Dashboard/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="i-dashboard">
+
+      </div>
+    );
+  }
+}
+
+export default App;
